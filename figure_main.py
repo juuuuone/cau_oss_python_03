408bf96ce45ab2d98b688609d402576e9be04238
juuuuone <127924700+juuuuone@users.noreply.github.com>
2023-05-19 17:41:55 +0900

    Update figure_main.py

--- figure_main.py
+++ figure_main.py
@@ -1,1 +1,20 @@
 
+# %%
+
+# [fill this line]
+
+import figure
+
+
+myline = figure.line(10)
+
+square = myline.area_square(myline.get_length())
+print(square)
+
+myline.set_length(20)
+regular_triangle = myline.area_regular_triangle(myline.get_length())
+print(regular_triangle)
+
+myline.set_length(30)
+circle = myline.area_circle(myline.get_length())
+print(circle)
